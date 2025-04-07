8cc6d045b5311c4ddf5b0e9ad021ad78fe789de8
SUHADA BINTI SAMSUDIN <suhada.samsudin3110@gmail.com>
2025-04-07 09:27:39 +0800

    Job,organization,qualification, Nationalities, Corporate Branding v1

--- navigation/index.js
+++ navigation/index.js
@@ -12,55 +12,130 @@ export default [
       }
     ]
   },
+  // {
+  //   "header": "Administration",
+  //   "description": "Manage your application",
+  //   "child": [
+  //     {
+  //       "title": "Configuration",
+  //       "icon": "ic:outline-settings",
+  //       "child": [
+  //         {
+  //           "title": "Environment",
+  //           "path": "/devtool/config/environment"
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       "title": "Menu Editor",
+  //       "icon": "ci:menu-alt-03",
+  //       "path": "/devtool/menu-editor",
+  //       "child": []
+  //     },
+  //     {
+  //       "title": "Manage Users",
+  //       "path": "/devtool/user-management",
+  //       "icon": "ph:user-circle-gear",
+  //       "child": [
+  //         {
+  //           "title": "User List",
+  //           "path": "/devtool/user-management/user",
+  //           "icon": "",
+  //           "child": []
+  //         },
+  //         {
+  //           "title": "Role List",
+  //           "path": "/devtool/user-management/role",
+  //           "icon": "",
+  //           "child": []
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       "title": "Content",
+  //       "icon": "mdi:pencil-ruler",
+  //       "child": [
+  //         {
+  //           "title": "Editor",
+  //           "path": "/devtool/content-editor"
+  //         },
+  //         {
+  //           "title": "Template",
+  //           "path": "/devtool/content-editor/template"
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       "title": "API Editor",
+  //       "path": "/devtool/api-editor",
+  //       "icon": "material-symbols:api-rounded",
+  //       "child": []
+  //     },
+  //     {
+  //       "title": "Code Playground",
+  //       "path": "/devtool/code-playground",
+  //       "icon": "mdi:code-braces",
+  //       "child": []
+  //     }
+  //   ],
+  //   "meta": {
+  //     "auth": {
+  //       "role": [
+  //         "Developer"
+  //       ]
+  //     }
+  //   }
+  // },
   {
-    "header": "Administration",
-    "description": "Manage your application",
-    "child": [
+    "header": "Naim Works",
+    "description": "",
+    child: [
       {
-        "title": "Configuration",
-        "icon": "ic:outline-settings",
-        "child": [
+        title: "Qualification",
+        icon:"healthicons:i-exam-qualification-outline",
+        child: [
           {
-            "title": "Environment",
-            "path": "/devtool/config/environment"
-          }
-        ]
-      },
-      {
-        "title": "Menu Editor",
-        "icon": "ci:menu-alt-03",
-        "path": "/devtool/menu-editor",
-        "child": []
-      },
-      {
-        "title": "Manage Users",
-        "path": "/devtool/user-management",
-        "icon": "ph:user-circle-gear",
-        "child": [
-          {
-            "title": "User List",
-            "path": "/devtool/user-management/user",
-            "icon": "",
-            "child": []
+            title: "Skills",
+            path: "/admin/qualifications/skills",
           },
           {
-            "title": "Role List",
-            "path": "/devtool/user-management/role",
-            "icon": "",
-            "child": []
-          }
-        ]
+            title: "Education",
+            path: "/admin/qualifications/educations",
+          },
+          {
+            title: "Licenses",
+            path: "/admin/qualifications/licenses",
+          },
+          {
+            title: "Languages",
+            path: "/admin/qualifications/languages",
+          },
+          {
+            title: "Memberships",
+            path: "/admin/qualifications/memberships",
+          },
+        ],
       },
-
       {
-        "title": "Admin",
-        "path": "/orangeHR/jobtitles",
-        "icon": "ph:user-circle-gear",
-        "child": [
+        title: "Race & Religion",
+        icon:"ic:baseline-settings",
+        path: "/admin/raceReligion",
+      },
+      {
+        title: "Coparate Branding",
+        icon:"streamline:interface-text-formatting-paragraph-bullets-points-bullet-align-paragraph-formatting-bullets-text",
+        path: "/admin/coparateBranding",
+      },
+    ],
+  },
+  {
+    "header": "Aqib Works",
+    "description": "",
+    "child": [
           {
             "title": "Job",
             "path": "/orangeHR/jobtitles",
-            "icon": "",
+            "icon": "ic:sharp-list-alt",
             "child": [
               {
                 "title": "Job Title",
@@ -97,7 +172,7 @@ export default [
           {
             "title": "Organization",
             "path": "/orangeHR/organization",
-            "icon": "ph:user-circle-gear",
+            "icon": "ri:organization-chart",
             "child": [
               {
                 "title": "General Information",
@@ -126,104 +201,30 @@ export default [
             ]
           }
         ]
-      },
-      {
-        title: "Admin",
-        icon: "ic:outline-insert-drive-file",
-        child: [
-          {
-            title: "Qualification",
-            child: [
-              {
-                title: "Skills",
-                path: "/admin/qualifications/skills",
-              },
-              {
-                title: "Education",
-                path: "/admin/qualifications/educations",
-              },
-              {
-                title: "Licenses",
-                path: "/admin/qualifications/licenses",
-              },
-              {
-                title: "Languages",
-                path: "/admin/qualifications/languages",
-              },
-              {
-                title: "Memberships",
-                path: "/admin/qualifications/memberships",
-              },
-            ],
-          },
-          {
-            title: "Race & Religion",
-            path: "/admin/raceReligion",
-          },
-          {
-            title: "Coparate Branding",
-            path: "/admin/coparateBranding",
-          },
-        ],
-      },
-      {
-        "title": "Content",
-        "icon": "mdi:pencil-ruler",
-        "child": [
-          {
-            "title": "Editor",
-            "path": "/devtool/content-editor"
-          },
-          {
-            "title": "Template",
-            "path": "/devtool/content-editor/template"
-          }
-        ]
-      },
-      {
-        "title": "API Editor",
-        "path": "/devtool/api-editor",
-        "icon": "material-symbols:api-rounded",
-        "child": []
-      },
-      {
-        "title": "Code Playground",
-        "path": "/devtool/code-playground",
-        "icon": "mdi:code-braces",
-        "child": []
-      }
-    ],
-    "meta": {
-      "auth": {
-        "role": [
-          "Developer"
-        ]
-      }
-    }
   },
-  {
-    "header": "Help",
-    "description": "Help and documentation",
-    "child": [
-      {
-        "title": "Documentation",
-        "icon": "solar:book-bookmark-minimalistic-bold",
-        "path": "https://manual.corrad.ai",
-        "external": true
-      },
-      {
-        "title": "UI Components",
-        "icon": "material-symbols:settings-input-component-outline-rounded",
-        "path": "https://ui.corrad.ai",
-        "external": true
-      }
-    ],
-    "meta": {
-      "auth": {
-        "role": [
-          "Developer"
-        ]
-      }
-    }
-  }
+  // {
+  //   "header": "Help",
+  //   "description": "Help and documentation",
+  //   "child": [
+  //     {
+  //       "title": "Documentation",
+  //       "icon": "solar:book-bookmark-minimalistic-bold",
+  //       "path": "https://manual.corrad.ai",
+  //       "external": true
+  //     },
+  //     {
+  //       "title": "UI Components",
+  //       "icon": "material-symbols:settings-input-component-outline-rounded",
+  //       "path": "https://ui.corrad.ai",
+  //       "external": true
+  //     }
+  //   ],
+  //   "meta": {
+  //     "auth": {
+  //       "role": [
+  //         "Developer"
+  //       ]
+  //     }
+  //   }
+  // }
 ];
